fb2c8e9322db15f236530867975ed3684391e124
Hunter Clarke <hunter@hunterclarke.me>
2023-07-25 16:19:30 -0600

    removes usage of ssl setting

--- php/nginx/src/index.php
+++ php/nginx/src/index.php
@@ -1,8 +1,6 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 
-\Spackle\Spackle::setSSLEnabled(false);
-
 function request_path()
 {
     $request_uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
